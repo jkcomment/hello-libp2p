301faafd77eb84c35225ae4d2edb92273c277142
jkcomment <jkcomment@gmail.com>
2019-04-19 19:00:26 +0900

    add ping

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const multiaddr = require('multiaddr')
 const PeerInfo = require('peer-info')
 const P2PNode = require('./p2p')
+const process = require('process')
+const PeerId = require('peer-id')
+const Ping = require('libp2p-ping')
 
 function createPeer(callback) {
     // 新しく生成されたPeerIdを使って新しいPeerInfoオブジェクトを生成
@@ -26,7 +29,30 @@ function createPeer(callback) {
 function handleStart(peer) {
     const addresses = peer.peerInfo.multiaddrs.toArray()
     console.log("### peer started. listening on addresses: ")
-    addresses.forEach(addr => console.log(addr.toString()))
+    addresses.forEach(addr => {
+        console.log(addr.toString())
+        pingRemotePeer(peer, addr)
+    })
+}
+
+function pingRemotePeer(localPeer, addr) {
+    if (process.argv.length < 3) {
+        return console.log("### no remote peer address given, skipping ping")
+    }
+
+    const remoteAddr = multiaddr(process.argv[2])
+    const peerId = PeerId.createFromB58String(addr.getPeerId())
+    const remotePeerInfo = new PeerInfo(peerId)
+    remotePeerInfo.multiaddrs.add(addr)
+
+    console.log("### pinging remote peer at ", remoteAddr.toString())
+    localPeer.ping(remotePeerInfo, (err, time) => {
+        if (err) {
+            return console.error("### error pinging: ", err)
+        }
+
+        console.log(`### pinged ${remoteAddr.toString()} in ${time}ms`)
+    })
 }
 
 createPeer((err, peer) => {
--- src/p2p/index.js
+++ src/p2p/index.js
@@ -3,6 +3,7 @@ const TCP = require('libp2p-tcp')
 const defaultsDeep = require('@nodeutils/defaults-deep')
 const Multiplex = require('libp2p-mplex')
 const SECIO = require('libp2p-secio')
+const Ping = require('libp2p-ping')
 const DEFAULT_OPTS = {
     modules: {
         transport: [
@@ -18,7 +19,17 @@ const DEFAULT_OPTS = {
 }
 
 module.exports = class P2PNode extends Libp2p {
-  constructor (opts) {
-    super(defaultsDeep(opts, DEFAULT_OPTS))
-  }
+    constructor (opts) {
+        super(defaultsDeep(opts, DEFAULT_OPTS))
+    }
+
+    ping (remotePeerInfo, callback) {
+        const p = new Ping(this._switch, remotePeerInfo)
+        p.on('ping', time => {
+            p.stop()
+            callback(null, time)
+        })
+        p.on('error', callback)
+        p.start()
+    }
 }
